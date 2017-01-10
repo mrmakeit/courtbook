8977eacbb6b2e4e405943ab057809cc8bf350e5f
Ash Lux <arlux@monsanto.com>
2017-01-09 20:56:41 -0600

    Move logic out of routers to reminders.js.

---
+++ src/server/reminders.js
@@ -0,0 +1,53 @@
+const cases = {
+    FAKE1: {
+        defendants: [
+            {name: "Mickey Mouse"},
+            {name: "Minnie Mouse"}
+        ],
+        events: {
+            "Mickey Mouse": [
+                {date: "2016-12-30T05:49:20+00:00", description: "PRELIMINARY HEARING ISSUE (PUBLIC DEFENDER)"}
+            ],
+            "Minnie Mouse": [
+                {date: "2016-12-30T05:49:20+00:00", description: "PRELIMINARY HEARING ISSUE (PUBLIC DEFENDER)"},
+                {date: "2016-12-30T05:49:20+00:00", description: "HEARING #2"},
+                {date: "2016-12-30T05:49:20+00:00", description: "HEARING #3"}
+            ]
+        }
+    },
+    FAKE2: {
+        defendants: [
+            {name: "Donald Duck"},
+            {name: "Daisy Duck"}
+        ],
+        events: {
+            "Donald Duck": [],
+            "Daisy Duck": []
+        }
+    },
+    FAKE3: {
+        defendants: [
+            {name: "Scrooge McDuck"}
+        ],
+        events: {
+            "Scrooge McDuck": [
+                {date: "2016-12-30T05:49:20+00:00", description: "PRELIMINARY HEARING ISSUE (PUBLIC DEFENDER)"},
+                {date: "2016-12-30T05:49:20+00:00", description: "HEARING #2"},
+                {date: "2016-12-30T05:49:20+00:00", description: "HEARING #3"},
+                {date: "2016-12-30T05:49:20+00:00", description: "HEARING #4"},
+                {date: "2016-12-30T05:49:20+00:00", description: "HEARING #5"}
+            ]
+        }
+    }
+};
+
+export const getPeopleByCaseNumber = (caseNumber) => {
+    const theCase = cases[caseNumber];
+    return theCase ? theCase.defendants : [];
+};
+
+export const getReminderByCaseNumberAndName = (caseNumber, personName) => {
+    const events = cases[caseNumber] && cases[caseNumber].events ? cases[caseNumber].events[personName] : [];
+    return events ? events : [];
+};
+
--- src/server/v1Router.js
+++ src/server/v1Router.js
@@ -1,90 +1,12 @@
 import express from "express";
-import models from "./models";
-
-const cases = {
-    FAKE1: {
-        defendants: [
-            {name: "Mickey Mouse"},
-            {name: "Minnie Mouse"}
-        ],
-        events: {
-            "Mickey Mouse": [
-                {date: "2016-12-30T05:49:20+00:00", description: "PRELIMINARY HEARING ISSUE (PUBLIC DEFENDER)"}
-            ],
-            "Minnie Mouse": [
-                {date: "2016-12-30T05:49:20+00:00", description: "PRELIMINARY HEARING ISSUE (PUBLIC DEFENDER)"},
-                {date: "2016-12-30T05:49:20+00:00", description: "HEARING #2"},
-                {date: "2016-12-30T05:49:20+00:00", description: "HEARING #3"}
-            ]
-        }
-    },
-    FAKE2: {
-        defendants: [
-            {name: "Donald Duck"},
-            {name: "Daisy Duck"}
-        ],
-        events: {
-            "Donald Duck": [],
-            "Daisy Duck": []
-        }
-    },
-    FAKE3: {
-        defendants: [
-            {name: "Scrooge McDuck"}
-        ],
-        events: {
-            "Scrooge McDuck": [
-                {date: "2016-12-30T05:49:20+00:00", description: "PRELIMINARY HEARING ISSUE (PUBLIC DEFENDER)"},
-                {date: "2016-12-30T05:49:20+00:00", description: "HEARING #2"},
-                {date: "2016-12-30T05:49:20+00:00", description: "HEARING #3"},
-                {date: "2016-12-30T05:49:20+00:00", description: "HEARING #4"},
-                {date: "2016-12-30T05:49:20+00:00", description: "HEARING #5"}
-            ]
-        }
-    }
-};
+import {getPeopleByCaseNumber, getReminderByCaseNumberAndName} from "./reminders";
 
 const router = express.Router();
 
-export const getPeopleByCaseNumber = (req, res) => {
-    const {caseNumber} = req.params;
-    console.info("Getting people for case number: ", caseNumber);
-    const theCase = cases[caseNumber];
-    res.send(theCase ? theCase.defendants : []);
-};
-
-
-export const getNotifications = (req, res) => {
-    const {caseNumber, personName} = req.params;
-    console.info("Getting people for case number:", caseNumber, "and person:", personName);
-    const events = cases[caseNumber] && cases[caseNumber].events ? cases[caseNumber].events[personName] : [];
-    res.send(events ? events : []);
-};
-
-export const createReminders = (req, res) => {
-    const personReminders = req.body;
-    models.Person.create(personReminders)
-        .then(() => console.info("DONE") || res.send("WOOT"));
-};
-
 router.route('/v1/case/:caseNumber')
-    .get(getPeopleByCaseNumber);
+    .get((req, res) => res.send(getPeopleByCaseNumber(req.params.caseNumber)));
 
 router.route('/v1/case/:caseNumber/person/:personName')
-    .get(getNotifications);
-
-router.route('/v1/reminders')
-    .post(createReminders);
-
-router.route("/people")
-    .get((req, res) => {
-    models.Person.findAll({include: [models.Person.Notifications]}).then(data => res.send(data))
-});
-
-//
-// app.get("/notifications", (req, res) => {
-//     models.Notification.findAll({include: [models.Person]})
-//         .then(data => res.send(data))
-// });
+    .get((req, res) => res.send(getReminderByCaseNumberAndName(req.params.caseNumber, req.params.personName)));
 
 export default router;
